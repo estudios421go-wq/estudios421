52c75cddbf945d4cd26be59ff521f12cc0cee1f4
estudios421go-wq <estudios421go@gmail.com>
2025-12-27 17:36:09 -0500

    Solucion estructural: Añadido viewport meta tag para habilitar responsive

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head"; // Importamos Head para las etiquetas meta
 // Importación de los estilos base para que el Banner funcione
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Esta línea es la que soluciona el problema del tamaño en móviles */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
