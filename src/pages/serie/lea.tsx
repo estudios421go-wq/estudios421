6c9bc37541391c46b4c8ec45268200effec232b0
Estudios 421 Official <estudios421go@gmail.com>
2026-01-12 15:49:28 -0500

    Update lea.tsx

--- src/pages/serie/lea.tsx
+++ src/pages/serie/lea.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
 
+// IMPORTACIÓN DINÁMICA DE LOS TRES DISEÑOS
 const LeaPC = dynamic(() => import('../components/series/lea/LeaPC'));
 const LeaMobile = dynamic(() => import('../components/series/lea/LeaMobile'));
 const LeaTV = dynamic(() => import('../components/series/lea/LeaTV'));
