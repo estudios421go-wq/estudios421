eb084ca969b5e7eb636f5f4db097a59dd2b5b146
Estudios 421 Official <estudios421go@gmail.com>
2026-01-17 16:58:11 -0500

    Update LaVidaDeJobTV.tsx

--- src/components/series/la-vida-de-job/LaVidaDeJobTV.tsx
+++ src/components/series/la-vida-de-job/LaVidaDeJobTV.tsx
@@ -1,1 +1,7 @@
-
+import React from 'react';
+const LaVidaDeJobTV = () => (
+  <div className="bg-black min-h-screen flex items-center justify-center">
+    <h1 className="text-white text-xl font-bold uppercase tracking-widest">Versión TV en Desarrollo...</h1>
+  </div>
+);
+export default LaVidaDeJobTV;
