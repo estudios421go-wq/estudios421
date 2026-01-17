f2447e22485acb74227f4f52e384c7b255e711cb
Estudios 421 Official <estudios421go@gmail.com>
2026-01-17 16:57:51 -0500

    Update LaVidaDeJobMobile.tsx

--- src/components/series/la-vida-de-job/LaVidaDeJobMobile.tsx
+++ src/components/series/la-vida-de-job/LaVidaDeJobMobile.tsx
@@ -1,1 +1,7 @@
-
+import React from 'react';
+const LaVidaDeJobMobile = () => (
+  <div className="bg-black min-h-screen flex items-center justify-center">
+    <h1 className="text-white text-xl font-bold uppercase tracking-widest">Versión Móvil en Desarrollo...</h1>
+  </div>
+);
+export default LaVidaDeJobMobile;
