676dadbe3d9cd75fde957207f3ed87fd7357b08d
Estudios 421 Official <estudios421go@gmail.com>
2026-01-17 16:56:48 -0500

    Update la-vida-de-job.tsx

--- src/pages/serie/la-vida-de-job.tsx
+++ src/pages/serie/la-vida-de-job.tsx
@@ -1,1 +1,23 @@
+import React, { useState, useEffect } from 'react';
+// IMPORTACIONES CORREGIDAS CON LA NUEVA CARPETA
+import LaVidaDeJobPC from '../../components/series/la-vida-de-job/LaVidaDeJobPC';
+import LaVidaDeJobMobile from '../../components/series/la-vida-de-job/LaVidaDeJobMobile';
 
+const LaVidaDeJobPage = () => {
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const checkDevice = () => {
+      setIsMobile(window.innerWidth < 1024);
+    };
+    checkDevice();
+    window.addEventListener('resize', checkDevice);
+    return () => window.removeEventListener('resize', checkDevice);
+  }, []);
+
+  if (isMobile === null) return <div className="bg-black min-h-screen" />;
+
+  return isMobile ? <LaVidaDeJobMobile /> : <LaVidaDeJobPC />;
+};
+
+export default LaVidaDeJobPage;
