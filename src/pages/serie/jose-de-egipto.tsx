05d1ff2428d7563c1bc2fe315feecdfa1edb3bb8
Estudios 421 Official <estudios421go@gmail.com>
2026-01-20 14:01:15 -0500

    Update jose-de-egipto.tsx

--- src/pages/serie/jose-de-egipto.tsx
+++ src/pages/serie/jose-de-egipto.tsx
@@ -1,1 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
 
+const JoseDeEgiptoPC = dynamic(() => import('../../components/series/jose-de-egipto/JoseDeEgiptoPC'));
+const JoseDeEgiptoMobile = dynamic(() => import('../../components/series/jose-de-egipto/JoseDeEgiptoMobile'));
+const JoseDeEgiptoTV = dynamic(() => import('../../components/series/jose-de-egipto/JoseDeEgiptoTV'));
+
+const JoseDeEgiptoPage = () => {
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const checkDevice = () => {
+      setIsMobile(window.innerWidth < 1024);
+    };
+    checkDevice();
+    window.addEventListener('resize', checkDevice);
+    return () => window.removeEventListener('resize', checkDevice);
+  }, []);
+
+  if (isMobile === null) return <div className="bg-black min-h-screen" />;
+
+  return isMobile ? <JoseDeEgiptoMobile /> : <JoseDeEgiptoPC />;
+};
+
+export default JoseDeEgiptoPage;
