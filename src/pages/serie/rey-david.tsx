6e71f16c3a3fd4b667645c0e12262397060c28cf
Estudios 421 Official <estudios421go@gmail.com>
2026-01-22 18:11:25 -0500

    Update rey-david.tsx

--- src/pages/serie/rey-david.tsx
+++ src/pages/serie/rey-david.tsx
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
 
-const DavidPC = dynamic(() => import('../../components/series/el-rey-david/ElReyDavidPC'), { ssr: false });
-const DavidMobile = dynamic(() => import('../../components/series/el-rey-david/ElReyDavidMobile'), { ssr: false });
+const ReyDavidPC = dynamic(() => import('../../components/series/rey-david/ReyDavidPC'), { ssr: false });
+const ReyDavidMobile = dynamic(() => import('../../components/series/rey-david/ReyDavidMobile'), { ssr: false });
+const ReyDavidTV = dynamic(() => import('../../components/series/rey-david/ReyDavidTV'), { ssr: false });
 
 const ReyDavidPage = () => {
   const [isMobile, setIsMobile] = useState<boolean | null>(null);
+
   useEffect(() => {
-    const checkDevice = () => setIsMobile(window.innerWidth < 1024);
+    const checkDevice = () => {
+      setIsMobile(window.innerWidth < 1024);
+    };
     checkDevice();
     window.addEventListener('resize', checkDevice);
     return () => window.removeEventListener('resize', checkDevice);
   }, []);
 
   if (isMobile === null) return <div className="bg-black min-h-screen" />;
-  return isMobile ? <DavidMobile /> : <DavidPC />;
+
+  return isMobile ? <ReyDavidMobile /> : <ReyDavidPC />;
 };
+
 export default ReyDavidPage;
