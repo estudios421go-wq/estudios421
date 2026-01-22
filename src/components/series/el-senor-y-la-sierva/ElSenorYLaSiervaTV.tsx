a70079f68150ddbe2bc3d1933cfe94a8b04a8677
Estudios 421 Official <estudios421go@gmail.com>
2026-01-22 18:51:10 -0500

    Update ElSenorYLaSiervaTV.tsx

--- src/components/series/el-senor-y-la-sierva/ElSenorYLaSiervaTV.tsx
+++ src/components/series/el-senor-y-la-sierva/ElSenorYLaSiervaTV.tsx
@@ -1,1 +1,7 @@
-
+import React from 'react';
+const ElSeñorYLaSiervaTV = () => (
+  <div className="bg-black min-h-screen flex items-center justify-center text-center">
+    <h1 className="text-white text-2xl font-bold uppercase tracking-[0.5em]">Versión TV en Desarrollo...</h1>
+  </div>
+);
+export default ElSeñorYLaSiervaTV;
