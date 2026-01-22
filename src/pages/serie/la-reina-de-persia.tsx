dfb4505fb72afd4b15ba5a285ac9d3049d7e0f40
Estudios 421 Official <estudios421go@gmail.com>
2026-01-22 18:05:32 -0500

    Update la-reina-de-persia.tsx

--- src/pages/serie/la-reina-de-persia.tsx
+++ src/pages/serie/la-reina-de-persia.tsx
@@ -1,1 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
 
+const PersiaPC = dynamic(() => import('../../components/series/la-reina-de-persia/LaReinaDePersiaPC'), { ssr: false });
+const PersiaMobile = dynamic(() => import('../../components/series/la-reina-de-persia/LaReinaDePersiaMobile'), { ssr: false });
+const PersiaTV = dynamic(() => import('../../components/series/la-reina-de-persia/LaReinaDePersiaTV'), { ssr: false });
+
+const ReinaDePersiaPage = () => {
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+  useEffect(() => {
+    const checkDevice = () => setIsMobile(window.innerWidth < 1024);
+    checkDevice();
+    window.addEventListener('resize', checkDevice);
+    return () => window.removeEventListener('resize', checkDevice);
+  }, []);
+  if (isMobile === null) return <div className="bg-black min-h-screen" />;
+  return isMobile ? <PersiaMobile /> : <PersiaPC />;
+};
+export default ReinaDePersiaPage;
