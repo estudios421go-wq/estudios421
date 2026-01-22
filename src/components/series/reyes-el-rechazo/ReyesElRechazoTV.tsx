cc66efc5edbeea2a4b635628611df2ed6b09de61
Estudios 421 Official <estudios421go@gmail.com>
2026-01-22 18:34:46 -0500

    Update ReyesElRechazoTV.tsx

--- src/components/series/reyes-el-rechazo/ReyesElRechazoTV.tsx
+++ src/components/series/reyes-el-rechazo/ReyesElRechazoTV.tsx
@@ -1,1 +1,7 @@
-
+import React from 'react';
+const ReyesElRechazoTV = () => (
+  <div className="bg-black min-h-screen flex items-center justify-center">
+    <h1 className="text-white text-2xl font-bold uppercase tracking-[0.5em]">Versión TV en Desarrollo...</h1>
+  </div>
+);
+export default ReyesElRechazoTV;
