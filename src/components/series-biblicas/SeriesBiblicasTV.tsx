2a6723ffd9e060c16d84782f742035e0734ec25f
Estudios 421 Official <estudios421go@gmail.com>
2026-01-23 11:02:34 -0500

    Create SeriesBiblicasTV.tsx

---
+++ src/components/series-biblicas/SeriesBiblicasTV.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const SeriesBiblicasTV = () => (
+  <div className="bg-black min-h-screen flex items-center justify-center text-white text-xs uppercase tracking-widest">
+    Catálogo TV en desarrollo
+  </div>
+);
+export default SeriesBiblicasTV;
