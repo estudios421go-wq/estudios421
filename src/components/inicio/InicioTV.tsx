36171d6a00d8cdd612120728f56037042c3c7f14
Estudios 421 Official <estudios421go@gmail.com>
2026-01-23 10:46:19 -0500

    Create InicioTV.tsx

---
+++ src/components/inicio/InicioTV.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InicioTV = () => (
+  <div className="bg-black min-h-screen flex items-center justify-center text-center p-20">
+    <div>
+      <h1 className="text-[#F09800] text-5xl font-black uppercase mb-4">Modo TV</h1>
+      <p className="text-white text-xl tracking-[0.5em] opacity-50">SISTEMA EN DESARROLLO</p>
+    </div>
+  </div>
+);
+
+export default InicioTV;
