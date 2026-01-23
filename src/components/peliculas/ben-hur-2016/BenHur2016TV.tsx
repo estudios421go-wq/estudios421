79432c07f3b6e59a3a6ca15be7ca6f7e73744661
Estudios 421 Official <estudios421go@gmail.com>
2026-01-23 10:03:19 -0500

    Create BenHur2016TV.tsx

---
+++ src/components/peliculas/ben-hur-2016/BenHur2016TV.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const BenHur2016TV = () => (
+  <div className="bg-black min-h-screen flex items-center justify-center text-center">
+    <h1 className="text-white text-2xl font-bold uppercase tracking-[0.5em]">Versión TV en Desarrollo...</h1>
+  </div>
+);
+export default BenHur2016TV;
