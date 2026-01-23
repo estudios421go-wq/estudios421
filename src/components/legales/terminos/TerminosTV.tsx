19b9cf7feb4b23d3ab858dcd86d6c4fe9a9ee733
Estudios 421 Official <estudios421go@gmail.com>
2026-01-23 11:39:39 -0500

    Create TerminosTV.tsx

---
+++ src/components/legales/terminos/TerminosTV.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const TerminosTV = () => (
+  <div className="bg-black min-h-screen flex flex-col items-center justify-center p-20 text-center">
+    <h1 className="text-[#F09800] text-6xl font-black uppercase mb-4">Términos de Uso</h1>
+    <p className="text-white text-2xl max-w-5xl leading-loose">Plataforma sin fines de lucro destinada a la educación y valores bíblicos. Respeta los derechos de autor.</p>
+  </div>
+);
+export default TerminosTV;
