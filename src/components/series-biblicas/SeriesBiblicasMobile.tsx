550ef7a40cd1643de26a81d8a413b624b6721f28
Estudios 421 Official <estudios421go@gmail.com>
2026-01-23 11:02:17 -0500

    Create SeriesBiblicasMobile.tsx

---
+++ src/components/series-biblicas/SeriesBiblicasMobile.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const SeriesBiblicasMobile = () => (
+  <div className="bg-black min-h-screen flex items-center justify-center text-white text-xs uppercase tracking-widest">
+    Catálogo Móvil en desarrollo
+  </div>
+);
+export default SeriesBiblicasMobile;
