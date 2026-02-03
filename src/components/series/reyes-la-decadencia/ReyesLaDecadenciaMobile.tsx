3c9a0f8c41cbd5ea2d45b2f5a308aee3ccd47673
Estudios 421 Official <estudios421go@gmail.com>
2026-02-03 18:25:31 -0500

    Update ReyesLaDecadenciaMobile.tsx

--- src/components/series/reyes-la-decadencia/ReyesLaDecadenciaMobile.tsx
+++ src/components/series/reyes-la-decadencia/ReyesLaDecadenciaMobile.tsx
@@ -12,7 +12,7 @@ import { allSeries } from '../../../data/series';
 const reyesDecadenciaEpisodes = [
   { id: 1, title: "EL DÍA SIGUIENTE", dur: "48:11", thumb: "https://static.wixstatic.com/media/859174_07288c0a3d8e4891b810a15ce13ba86b~mv2.jpg", url: "https://ok.ru/videoembed/14918896323072", available: true },
   { id: 2, title: "LA VENGANZA DEL DUELO", dur: "42:15", thumb: "https://static.wixstatic.com/media/859174_c6e9b142404b4136be7ef6b7afb8f553~mv2.jpg", url: "https://ok.ru/videoembed/14918896454144", available: true },
-  { id: 3, title: "ESTRENO 3 DE FEBRERO", available: false },
+  { id: 3, title: "INTENTO DE COMENZAR DE NUEVO", dur: "51:45", thumb: "https://static.wixstatic.com/media/859174_b0de23d52b1244398f856ca3e330795f~mv2.jpg", url: "https://ok.ru/videoembed/14945256147456", available: true },
   { id: 4, title: "ESTRENO 4 DE FEBRERO", available: false },
   { id: 5, title: "ESTRENO 5 DE FEBRERO", available: false },
   { id: 6, title: "ESTRENO 6 DE FEBRERO", available: false },
