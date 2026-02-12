1bf3b4ea5a015cb4d97f2495f35ba67e3db4f814
Estudios 421 Official <estudios421go@gmail.com>
2026-02-12 18:10:34 -0500

    Create MiraclesOfJesusEN_TV.tsx

---
+++ src/components/intl-en/series/miracles-of-jesus/MiraclesOfJesusEN_TV.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MiraclesOfJesusEN_TV = () => (
+  <div className="bg-black min-h-screen flex items-center justify-center text-center">
+    <h1 className="text-white text-2xl font-bold uppercase tracking-[0.5em]">TV Version Under Development...</h1>
+  </div>
+);
+
+export default MiraclesOfJesusEN_TV;
