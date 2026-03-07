71d2287d23fc9cb192eb1b23bc374a1e072446d1
Estudios 421 Official <estudios421go@gmail.com>
2026-03-07 10:42:18 -0500

    Update Apocalipsis88TV.tsx

--- src/components/series/apocalipsis88/Apocalipsis88TV.tsx
+++ src/components/series/apocalipsis88/Apocalipsis88TV.tsx
@@ -1,1 +1,9 @@
+import React from 'react';
 
+const Apocalipsis88TV = () => (
+  <div className="bg-black min-h-screen flex items-center justify-center text-center">
+    <h1 className="text-white text-2xl font-bold uppercase tracking-[0.5em]">Apocalipsis (88) - Versión TV en Desarrollo...</h1>
+  </div>
+);
+
+export default Apocalipsis88TV;
