83683c8a53866fec50079588eb2a6ff5f614e2d6
Estudios 421 Official <estudios421go@gmail.com>
2026-03-31 14:49:15 -0500

    Update TheChosenTV.tsx

--- src/components/series-tv/the-chosen/TheChosenTV.tsx
+++ src/components/series-tv/the-chosen/TheChosenTV.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
 
+const TheChosenTV = () => {
+  return (
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-white font-sans text-xl uppercase tracking-widest">
+        La versión para Smart TV estará disponible pronto.
+      </h1>
+    </div>
+  );
+};
+
+export default TheChosenTV;
